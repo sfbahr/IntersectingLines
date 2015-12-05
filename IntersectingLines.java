ca2340746944b45a9c7eea2106a7d174fbeab3ce
anuj1 <anuj1@vt.edu>
2015-12-05 16:41:25 -0500

    Add the main class and the random line class

---
+++ IntersectingLines.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+/**
+ * @author Anuj
+ *
+ */
+public class IntersectingLines {
+
+	private File outputFile;
+	private static final int lineSegments = 10;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//createRandomLines(lineSegments);
+	}
+
+}
---
+++ RandomLines.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author Anuj
+ *
+ */
+public class RandomLines {
+
+	private Random r;
+	private int numberOfLines = 0;
+	private final int upperBound = 999975;
+
+	/**
+	 * 
+	 */
+	public RandomLines(int numberOfLines) {
+		r = new Random();
+		this.numberOfLines = numberOfLines;
+	}
+
+	public ArrayList<Line> lines() {
+		ArrayList<Line> lineSegments = new ArrayList<Line>(numberOfLines*2);
+		for (int i = 0; i < numberOfLines; i++) {
+			int randomIntX = r.nextInt(upperBound);
+			int randomIntY = r.nextInt(upperBound);
+			Line horizontalLine = new Line(new Point(randomIntX, randomIntY),
+					new Point(randomIntX + 25, randomIntY));
+			lineSegments.add(horizontalLine);
+			randomIntX = r.nextInt(upperBound);
+			randomIntY = r.nextInt(upperBound);
+			Line verticalLine = new Line(new Point(randomIntX, randomIntY),
+					new Point(randomIntX + 25, randomIntY));
+			lineSegments.add(verticalLine);
+		}
+		return lineSegments;
+	}
+}
