57aa80ea4b2f88fa0d2fb7d755593a3638077f5c
anuj1 <anuj1@vt.edu>
2015-12-05 19:48:30 -0500

    Prints out the table

--- IntersectingLines.java
+++ IntersectingLines.java
@@ -13,6 +13,7 @@ public class IntersectingLines {
 	public static void main(String[] args) {
 		
 		int[] lineSegments = {10, 100, 500, 1000, 2000, 10000, 20000};
+		System.out.println("Line Segments (n) \tBrute Force \tOur Algorithm \tIntersections");
 		for (int i = 0; i < lineSegments.length; i++) {
 			RandomLines r = new RandomLines(lineSegments[i]);
 			Line[] lines = r.lines();
@@ -21,10 +22,8 @@ public class IntersectingLines {
 			long startTime1 = System.currentTimeMillis();
 			BruteForceAlgorithm bruteForce = new BruteForceAlgorithm(lines);
 			LinkedList<Point> intersections = bruteForce.findIntersects();
-			System.out.println(intersections);
 			long endTime1 = System.currentTimeMillis();
 			long totalTime1 = endTime1 - startTime1;
-			System.out.println(totalTime1);
 			
 			// Our Algorithm
 			long startTime2 = System.currentTimeMillis();
@@ -32,6 +31,9 @@ public class IntersectingLines {
 			//do more work here
 			long endTime2 = System.currentTimeMillis();
 			long totalTime2 = endTime1 - startTime1;
+			
+			System.out.println(lineSegments[i] + "\t\t\t" + totalTime1 + "\t\t\t\t" + intersections);
+			
 		}
 		
 		
