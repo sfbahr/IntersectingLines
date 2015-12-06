341cf0457ccd987d17c90d0d458fc63df67cc14e
Max <Max>
2015-12-05 19:12:38 -0500

    Add timers to main

--- IntersectingLines.java
+++ IntersectingLines.java
@@ -17,7 +17,17 @@ public class IntersectingLines {
 		
 		RandomLines r = new RandomLines(lineSegments);
 		Line[] lines = r.lines();
+		
+		long startTime1 = System.currentTimeMillis();
+		//brute force algorithm
+		long endTime1 = System.currentTimeMillis();
+		long totalTime1 = endTime1 - startTime1;
+		
+		long startTime2 = System.currentTimeMillis();
 		Arrays.sort(lines);
+		//do more work here
+		long endTime2 = System.currentTimeMillis();
+		long totalTime2 = endTime1 - startTime1;
 		
 	}
 
