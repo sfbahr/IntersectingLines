bea6728690c34e9c2ae7f30797b7a366616cf35a
sfbahr <sfbahr@gmail.com>
2015-12-07 16:45:29 -0500

    Implement findIntersects in ProposedAlgorithm
    Also remove lines as a field since it's only used within the Event
    generation method.

--- ProposedAlgorithm.java
+++ ProposedAlgorithm.java
@@ -1,49 +1,56 @@
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ProposedAlgorithm {
-	private Line[] lines;
 	private BPTree tree;
 	private Event[] events;
 	
 	public ProposedAlgorithm(Line[] lines) {
-		this.lines = lines;
 		this.tree = new BPTree(true);
 		this.events = createEvents(lines);
 		Arrays.sort(events);
 	}
 	
 	public Event[] createEvents(Line[] l) {
-		events = new Event[l.length*2];
+		Event[] evts = new Event[l.length*3/2];
 		int i = 0;
 		for (Line currentLine: l) {
 			if (currentLine.isVertical()) {
-				events[i] = new Event(currentLine, true);
+				evts[i] = new Event(currentLine, true);
 				i++;
-				events[i] = new Event(currentLine, false);
+				evts[i] = new Event(currentLine, false);
 				i++;
 			}
 			else {
-				events[i] = new Event(currentLine, true); //it doesn't really matter what the second parameter is for a horizontal line
+				evts[i] = new Event(currentLine, true); //it doesn't really matter what the second parameter is for a horizontal line
 				i++;
 			}
 		}
-		return events;
+		return evts;
 	}
 	
 	public LinkedList<Point> findIntersects() {
 		LinkedList<Point> list = new LinkedList<Point>();
-		for (int i = 0; i < lines.length; i++) {
+		for (int i = 0; i < events.length; i++) {
 			
-			Line curLine = lines[i];
+			Event curEvent = events[i];
 			
-			if (curLine.isVertical()) {
-				VerticalLine vertLine = (VerticalLine) curLine;
-				
-				tree.insert(new KVPair<VerticalLine>(vertLine));
+			if (curEvent.isVertical()) {
+				VerticalLine vertLine = new VerticalLine(curEvent.getLine());
+				KVPair<VerticalLine> lineWrapper = new KVPair<>(vertLine);
+				if (curEvent.isLesser()) {
+					tree.insert(lineWrapper);
+				}
+				else {
+					tree.delete(lineWrapper);
+				}
 			}
 			else {
-				
+				List<Point> intersections = tree.findRange(curEvent.getLine().getLesser().getX(), 
+						curEvent.getLine().getGreater().getX(),
+						curEvent.getY());
+				list.addAll(intersections);
 			}
 		}
 		return list;
